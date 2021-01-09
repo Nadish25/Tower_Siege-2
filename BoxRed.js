c0ffd0010be9a316d71afdcc8c71c642dc472702
Nadish25 <75306433+Nadish25@users.noreply.github.com>
2021-01-09 17:55:11 +0530

    Add files via upload

---
+++ BoxRed.js
@@ -0,0 +1,56 @@
+class BoxRed{
+
+    constructor(x,y,width,height)
+    {
+        
+        var options={
+            'restitution':0.5,
+            'friction':0.5,
+            'density' :1.0,
+            'isStatic':false
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options);
+  
+        this.width=width;
+        this.height=height
+        World.add(world,this.body);
+        this.visibility=255;
+  
+    }
+  
+    display()
+    {
+        var pos=this.body.position;
+        var angle=this.body.angle;
+  
+        if(this.body.speed<3){
+            push();
+        translate(pos.x,pos.y);
+        angleMode(RADIANS);
+        rotate (angle);
+        stroke(0)
+        strokeWeight(1)
+        fill(237,85,101);
+        rectMode(CENTER);
+        rect(0,0,this.width,this.height);
+        pop();
+          }
+            else{
+                World.remove(world,this.body);
+                push();
+                this.visibility=this.visibility-5;
+                tint (255,this.visibility);
+                rect(this.image,this.body.position.x,this.body.position.y,50,50)
+                pop();
+    
+            }
+        
+
+        
+    }
+  
+  
+  
+  
+  };
+  
